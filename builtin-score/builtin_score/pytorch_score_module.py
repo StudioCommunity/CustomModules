370c216c31f1898b3a01ef06876abcedde18b180
Richard Zhao <zhwei@microsoft.com>
2019-07-24 18:03:53 +0800

    'typo'

--- builtin-score/builtin_score/pytorch_score_module.py
+++ builtin-score/builtin_score/pytorch_score_module.py
@@ -87,13 +87,13 @@ class PytorchWrapper(object):
                 input_params = []
                 logger.info(f"ROW = \n {row}")
                 print(f"ROW = \n {row}")
-                if self.is_image(row)
+                if self.is_image(row):
                     input_params.append(torch.Tensor(row).to(self.device))
                     print(f"IMAGE: {input_params}")
                 else:
                     for entry in row:
                         input_params.append(torch.Tensor(entry).to(self.device))
-                    print(f"FEATURES" {input_params})
+                    print(f"FEATURES: {input_params}")
                 predicted = self.model(*input_params)
                 output.append(predicted.cpu().numpy())
         return pd.DataFrame(output)
