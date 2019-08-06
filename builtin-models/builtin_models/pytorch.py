008f27c1e729a3fbb26de799b6e4af5451384e6c
Xin Zou (AI PLATFORM) <xinzou@microsoft.com>
2019-08-06 14:42:01 +0800

    add comments for save_model method

--- builtin-models/builtin_models/pytorch.py
+++ builtin-models/builtin_models/pytorch.py
@@ -1,6 +1,8 @@
 import os
 import yaml
 import cloudpickle
+import torch
+import torchvision
 
 from builtin_models.environment import _generate_conda_env
 from builtin_models.environment import _generate_ilearner_files
@@ -13,9 +15,6 @@ model_spec_file_name = "model_spec.yml"
 
 
 def _get_default_conda_env():
-    import torch
-    import torchvision
-
     return _generate_conda_env(
         additional_pip_deps=[
             "torch=={}".format(torch.__version__),
@@ -58,16 +57,22 @@ def _save_model(pytorch_model, path):
         cloudpickle.dump(pytorch_model, fp)
 
 
-def load_model_from_local_file(path):
+def _load_model_from_local_file(path):
     with open(path, 'rb') as fp:
         model = cloudpickle.load(fp)
     return model
 
 
-def save_model(pytorch_model, path, conda_env=None):
-    import torch
-    import torchvision
+def save_model(pytorch_model, path='./model/', conda_env=None):
+    """
+    Save a PyTorch model to a path on the local file system.
 
+    :param pytorch_model: PyTorch model to be saved. 
+
+    :param path: Path to a file or directory containing model data.
+
+    :param conda_env: Either a dictionary representation of a Conda environment or the path to a conda environment yaml file. 
+    """
     if(not path.endswith('/')):
         path += '/'
     if not os.path.exists(path):
--- builtin-models/test/builtin_models_test.py
+++ builtin-models/test/builtin_models_test.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     # keras test
     from builtin_models.keras import *
     print('---keras test---')
-    model = load_model_from_local_file('D:/GIT/CustomModules-migu-NewYamlTest2/dstest/model/keras-mnist/model.h5')
+    model = _load_model_from_local_file('D:/GIT/CustomModules-migu-NewYamlTest2/dstest/model/keras-mnist/model.h5')
     print('------')
     save_model(model, "./test/outputModels/keras/")
     print('********')
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     #sklearn test
     from builtin_models.sklearn import *
     print('---sklearn test---')
-    model = load_model_from_local_file('D:/GIT/CustomModules-migu-NewYamlTest2/dstest/dstest/sklearn/model/sklearn/model.pkl')
+    model = _load_model_from_local_file('D:/GIT/CustomModules-migu-NewYamlTest2/dstest/dstest/sklearn/model/sklearn/model.pkl')
     print('------')
     save_model(model, "./test/outputModels/sklearn/")
     print('********')
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     #pytorch test
     from builtin_models.pytorch import *
     print('---pytorch test---')
-    model = load_model_from_local_file('D:/GIT/CustomModules-migu-NewYamlTest2/dstest/model/pytorch-mnist/model.pkl')
+    model = _load_model_from_local_file('D:/GIT/CustomModules-migu-NewYamlTest2/dstest/model/pytorch-mnist/model.pkl')
     print('------')
     save_model(model, "./test/outputModels/pytorch/")
     print('********')
