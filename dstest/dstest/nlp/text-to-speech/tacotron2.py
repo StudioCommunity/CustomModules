10f8e45a8e3c4c62ba6d908fdc26d2c49ef29cc4
Clement Wang <clwan@microsoft.com>
2019-08-20 14:38:44 +0800

    update python flavor

--- dstest/dstest/nlp/text-to-speech/tacotron2.py
+++ dstest/dstest/nlp/text-to-speech/tacotron2.py
@@ -5,27 +5,36 @@ import torch
 import builtin_models.python
 
 class Tacotron2Model(object):
-    def __init__(self):
+    def __init__(self, model_path):
+        self.device = 'cuda' # cuda only
+        
+        print('# device:', self.device)
         tacotron2 = torch.hub.load('nvidia/DeepLearningExamples:torchhub', 'nvidia_tacotron2')
-        tacotron2 = tacotron2.to('cuda')
+        tacotron2 = tacotron2.to(self.device)
         tacotron2.eval()
         print('# tacotron2 parameters:', sum(param.numel() for param in tacotron2.parameters()))
 
         waveglow = torch.hub.load('nvidia/DeepLearningExamples:torchhub', 'nvidia_waveglow')
         waveglow = waveglow.remove_weightnorm(waveglow)
-        waveglow = waveglow.to('cuda')
+        waveglow = waveglow.to(self.device)
         waveglow.eval()
         print('# waveglow parameters:', sum(param.numel() for param in waveglow.parameters()))
         
         self.tacotron2 = tacotron2
         self.waveglow = waveglow
     
-    def predict(self, sequence):
+    def predict(self, text):
+        # prep-rocessing
+        sequence = np.array(model.tacotron2.text_to_sequence(text, ['english_cleaners']))[None, :]
+        
+        # run the models
+        sequence = torch.from_numpy(sequence).to(device=self.device, dtype=torch.int64)
         with torch.no_grad():
             _, mel, _, _ = self.tacotron2.infer(sequence)
             audio = self.waveglow.infer(mel)
         return audio
 
+# python -m dstest.nlp.text-to-speech.tacotron2
 if __name__ == '__main__':
     model = Tacotron2Model()
     model_path = "model/tacotron2"
@@ -34,17 +43,14 @@ if __name__ == '__main__':
     model1 = builtin_models.python.load_model(model_path)
 
     text = "We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness."
-    # prep-rocessing
-    sequence = np.array(model.tacotron2.text_to_sequence(text, ['english_cleaners']))[None, :]
-    sequence = torch.from_numpy(sequence).to(device='cuda', dtype=torch.int64)
-    print(sequence.shape)
-
     # run the models
-    audios = model1.predict(sequence)
+    audios = model1.predict(text)
+    #print(audios.shape)
 
     # post-processing
     for index in range(len(audios)):
         audio = audios[index]
+        print(audio.shape)
         audio_numpy = audio.data.cpu().numpy()
         rate = 22050
         write(f"audio_{index}.wav", rate, audio_numpy)
