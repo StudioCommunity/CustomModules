ab62227d265eb4ac1e820723286f932bbc88e2c6
Ming Gu <migu@microsoft.com>
2019-08-30 12:43:48 +0800

    add dependency to builtin-model

--- builtin-models/setup.py
+++ builtin-models/setup.py
@@ -6,6 +6,10 @@ setup(
     version="0.0.1",
     description="builtin_models",
     packages=["builtin_models"],
+    install_requires=[
+          "cloudpickle",
+          "PyYAML"
+      ],
     author="Xin Zou",
     license="MIT",
     include_package_data=True,
