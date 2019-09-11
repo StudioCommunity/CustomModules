e0727befffe1fe9b8a7db649aa071952b38c8681
Han Wang <wanhan@microsoft.com>
2019-09-11 15:16:48 +0800

    work around for register: non port param name can not contain spaces

--- builtin-score/builtin_score/constants.py
+++ builtin-score/builtin_score/constants.py
@@ -1,4 +1,4 @@
-APPEND_SCORE_COLUMNS_TO_OUTPUT_KEY = "Append score columns to output"
+APPEND_SCORE_COLUMNS_TO_OUTPUT_KEY = "Append_score_columns_to_output"
 SCORED_LABEL_COL_NAME = "Scored Label"
 SERIALIZATION_METHOD_KEY = "serialization_format"
 MODEL_FILE_PATH_KEY = "model_file_path"
